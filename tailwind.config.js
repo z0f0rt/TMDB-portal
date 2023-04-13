0753a60c7daf13db08fb484bf3a7f22cf27f9a7d
z0f0rt <greenboots@bk.ru>
2023-04-14 01:11:18 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,46 @@
-const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,jsx,js}", "./public/**/*.html"],
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    fontFamily: {
-      comfortaa: ["Comfortaa", "sans-serif"],
-    },
-    colors: {
-      ...colors,
-      white: "#ffffff",
-      main: {
-        200: "#c8d8e4",
-        250: "#bccbd7",
-        300: "#88bae0",
-        500: "#2b6777",
+    extend: {
+      animation: {
+        'slide-left': 'slideLeft .5s cubic-bezier(0.165, 0.840, 0.440, 1.000) forwards',
+        'slide-right': 'slideRight .5s cubic-bezier(0.165, 0.840, 0.440, 1.000) forwards',
       },
-      gray: "#f2f2f2",
-      secondary: { 500: "#52ab98" },
+      keyframes: {
+        slideLeft: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        slideRight: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      dropShadow: {
+        dark: '0 0 10px #ffa7c4',
+        light: '0 0 10px #d23669',
+      },
+      colors: {
+        yellow: '#ffba08',
+        pink: {
+          100: '#ffa7c4',
+          900: '#d23669',
+        },
+        neutral: {
+          100: '#f8f9fa',
+          200: '#e9ecef',
+          300: '#dee2e6',
+          400: '#ced4da',
+          500: '#adb5bd',
+          600: '#6c757d',
+          700: '#495057',
+          800: '#343a40',
+          900: '#212529',
+        },
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
